5681c5177c05ba633840ef7e1535e563d69491c7
husker00a <58126169+husker00a@users.noreply.github.com>
2025-02-08 14:27:06 -0600

    simple smoothing

--- assignment1.py
+++ assignment1.py
@@ -1,23 +1,12 @@
 # Importing Modules
-import pandas as pd, numpy as np
-from prophet import Prophet
-import plotly.express as px
+from statsmodels.tsa.api import ExponentialSmoothing
+from statsmodels.tsa.api import SimpleExpSmoothing
+import pandas as pd
 
 #Preparing Training Data
 trainData = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
-trainData = trainData[['Timestamp','trips']]
-trainData.columns = ['ds', 'y']
-
-#Loading and fitting model
-model = Prophet(changepoint_prior_scale = 0.5)
-modelFit = model.fit(trainData)
-
-#Preparing Test Data
-testData = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv")
-testData = testData[['Timestamp']]
-testData.columns = ['ds']
-
-#Getting Forecasting Data
-forecast = model.predict(testData)
-
-pred = forecast['yhat']
+trainData = trainData['trips']
+model = SimpleExpSmoothing(trainData)
+modelFit = model.fit(smoothing_level=0.5,optimized=False)
+# modelFit = model.fit(optimized=True)
+pred = modelFit.forecast(744)
